bc2e264832e15ac91918ca389bcdf431fd59669f
RohitSahadevan <rohitsahadevan@gmail.com>
2018-02-16 23:40:25 +0530

    trial

--- SwiftNew/SwiftNew/ViewController.swift
+++ SwiftNew/SwiftNew/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         label1.text = "Vishnu"
+        //dfddv
            // Do any additional setup after loading the view, typically from a nib.
     }
 
